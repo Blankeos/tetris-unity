ef8d0ef7cf1330054b99da9d6c60b9b29ad6a8b1
Blankeos <carloantonioct@gmail.com>
2021-08-30 23:09:32 +0800

    Halfway done with the project

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
+
+    public Tile tile;
+    public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, boardSize);
+        }
+    }
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
+        for (int i = 0; i < this.tetrominoes.Length; i++)
+        {
+            this.tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominoes.Length);
+        TetrominoData data = this.tetrominoes[random];
+
+        this.activePiece.Initialize(this, spawnPosition, data);
+        Set(this.activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tile.color = piece.data.tileColor;
+            this.tilemap.SetTile(tilePosition, tile);
+        }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tile.color = piece.data.tileColor;
+            this.tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            if (this.tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void ClearLines()
+    {
+        RectInt bounds = this.Bounds;
+        int row = bounds.yMin;
+
+        while (row < bounds.yMax)
+        {
+
+        }
+    }
+
+    private bool IsLineFull(int row)
+    {
+        RectInt bounds = this.Bounds;
+
+        for (int col = bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+
+            if (!this.tilemap.HasTile(position))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
